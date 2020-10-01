7616ea56faee4a7f203c195f22bee42c08774ce4
René Jochum <rene@jochum.dev>
2020-10-01 18:42:08 +0200

    Replace own crypters with github.com/GehirnInc/crypt
    Signed-off-by: René Jochum <rene@jochum.dev>

--- basic.go
+++ basic.go
@@ -1,50 +1,23 @@
 package auth
 
 import (
-	"bytes"
 	"context"
-	"crypto/sha1"
-	"crypto/subtle"
-	"encoding/base64"
 	"errors"
 	"net/http"
-	"strings"
 
-	"golang.org/x/crypto/bcrypt"
+	"github.com/GehirnInc/crypt"
+
+	// Import all crypters supported
+	_ "github.com/GehirnInc/crypt/apr1_crypt"
+	_ "github.com/GehirnInc/crypt/md5_crypt"
+	_ "github.com/GehirnInc/crypt/sha256_crypt"
+	_ "github.com/GehirnInc/crypt/sha512_crypt"
 )
 
 type compareFunc func(hashedPassword, password []byte) error
 
 var (
 	errMismatchedHashAndPassword = errors.New("mismatched hash and password")
-
-	compareFuncs = []struct {
-		prefix  string
-		compare compareFunc
-	}{
-		{"", compareMD5HashAndPassword}, // default compareFunc
-		{"{SHA}", compareShaHashAndPassword},
-		// Bcrypt is complicated. According to crypt(3) from
-		// crypt_blowfish version 1.3 (fetched from
-		// http://www.openwall.com/crypt/crypt_blowfish-1.3.tar.gz), there
-		// are three different has prefixes: "$2a$", used by versions up
-		// to 1.0.4, and "$2x$" and "$2y$", used in all later
-		// versions. "$2a$" has a known bug, "$2x$" was added as a
-		// migration path for systems with "$2a$" prefix and still has a
-		// bug, and only "$2y$" should be used by modern systems. The bug
-		// has something to do with handling of 8-bit characters. Since
-		// both "$2a$" and "$2x$" are deprecated, we are handling them the
-		// same way as "$2y$", which will yield correct results for 7-bit
-		// character passwords, but is wrong for 8-bit character
-		// passwords. You have to upgrade to "$2y$" if you want sant 8-bit
-		// character password support with bcrypt. To add to the mess,
-		// OpenBSD 5.5. introduced "$2b$" prefix, which behaves exactly
-		// like "$2y$" according to the same source.
-		{"$2a$", bcrypt.CompareHashAndPassword},
-		{"$2b$", bcrypt.CompareHashAndPassword},
-		{"$2x$", bcrypt.CompareHashAndPassword},
-		{"$2y$", bcrypt.CompareHashAndPassword},
-	}
 )
 
 // BasicAuth is an authenticator implementation for 'Basic' HTTP
@@ -84,36 +57,13 @@ func (a *BasicAuth) CheckAuth(r *http.Request) string {
 // CheckSecret returns true if the password matches the encrypted
 // secret.
 func CheckSecret(password, secret string) bool {
-	compare := compareFuncs[0].compare
-	for _, cmp := range compareFuncs[1:] {
-		if strings.HasPrefix(secret, cmp.prefix) {
-			compare = cmp.compare
-			break
-		}
+	if !crypt.IsHashSupported(secret) {
+		return false
 	}
-	return compare([]byte(secret), []byte(password)) == nil
-}
 
-func compareShaHashAndPassword(hashedPassword, password []byte) error {
-	d := sha1.New()
-	d.Write(password)
-	if subtle.ConstantTimeCompare(hashedPassword[5:], []byte(base64.StdEncoding.EncodeToString(d.Sum(nil)))) != 1 {
-		return errMismatchedHashAndPassword
-	}
-	return nil
-}
-
-func compareMD5HashAndPassword(hashedPassword, password []byte) error {
-	parts := bytes.SplitN(hashedPassword, []byte("$"), 4)
-	if len(parts) != 4 {
-		return errMismatchedHashAndPassword
-	}
-	magic := []byte("$" + string(parts[1]) + "$")
-	salt := parts[2]
-	if subtle.ConstantTimeCompare(hashedPassword, MD5Crypt(password, salt, magic)) != 1 {
-		return errMismatchedHashAndPassword
-	}
-	return nil
+	crypter := crypt.NewFromHash(secret)
+	err := crypter.Verify(secret, []byte(password))
+	return err == nil
 }
 
 // RequireAuth is an http.HandlerFunc for BasicAuth which initiates
